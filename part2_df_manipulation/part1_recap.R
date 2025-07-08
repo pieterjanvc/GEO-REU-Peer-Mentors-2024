edbfc6337c0435fc46d4c43499cf6aac13177674
pieterjanvc <pieterjanvc@hotmail.com>
2025-07-08 09:06:25 -0400

    Minor revision and updates to Part 2 content

--- part2_df_manipulation/part1_recap.R
+++ part2_df_manipulation/part1_recap.R
@@ -1,17 +1,17 @@
-
-
 # A line that starts with a hash tag is a comment
 
-# Simple Data types
-
+# --- Simple Data types
 1
+
 2.5
+
 "hello world"
+
 TRUE
 
-# Grouped data
-
+# --- Grouped data
 c(1,2,3)
+
 c("item1", "item2")
 
 list(1, "test", FALSE)
@@ -22,20 +22,22 @@ list(x = c(1,2,3), y = c("A", "B", "C"), z = c(TRUE, FALSE, FALSE))
 
 data.frame(x = c(1,2,3), y = c("A", "B", "C"), z = c(TRUE, FALSE, FALSE))
 
-# Variables
-
+# --- Variables
 var1 = c(7.25,5.33,9.17)
+
 var1
 
-# Functions
+# --- Functions
 sort(var1)
+
 sort(var1, decreasing = TRUE)
 
-# Pipe
+# --- Pipe
 var1 |> sort() |> round(1)
 
-# Vector operations
-
+# --- Vector operations
 var1 + 3
+
 var1 + var1
+
 var1 / min(var1)
--- part2_df_manipulation/part2_exercise.R
+++ part2_df_manipulation/part2_exercise.R
@@ -2,6 +2,9 @@
 #....... PART 2 EXERCISE ....... 
 #................................
 
+# This exercise is competed in a regular R script, and can be attempted once you
+#  understand the basics or already have some R experience 
+
 # Load the dplyr library
 library(dplyr)
 
@@ -17,15 +20,15 @@ myData = iris
 # --- Part 1 ---
 
 # Create a new variable called `part1` which only contains the 
-# petal related columns and the name of the species 
+#  petal-related columns and the name of the species 
 part1 <- myData |> select(Species, Petal.Length, Petal.Width)
 
 # Filter the part1 dataframe further to only contain rows where the petal length
-# is 1.7 cm or more and the width is smaller than 2
+#  is 1.7 cm or more and the width is smaller than 2
 part1 <- part1 |> filter(Petal.Length >= 1.7 & Petal.Width < 2)
 
-# Summarise the data so for each species so you will have the mean petal length 
-# and width
+# Summarise the data such that for each species so you will have the mean petal 
+#  length and width
 part1 <- part1 |> group_by(Species) |> 
   summarise(mLength = mean(Petal.Length), mWidth = mean(Petal.Width)) |> 
   ungroup()
@@ -33,13 +36,14 @@ part1 <- part1 |> group_by(Species) |>
 # --- Part 2 ---
 
 # Starting again from the original myData variable, create a new variable called 
-# part2 which does not contain the "setosa" species
+#  part2 which does not contain the setosa species
 part2 <- myData |> filter(Species != c("setosa"))
 
 # Create a new column called longSepal that stores a boolean value (TRUE / FALSE) 
-# indicating whether for an observation the Sepal length is at least double the Sepal width 
+#  indicating whether for an observation the Sepal length is at least double 
+#  that of the Sepal width 
 part2 <- myData |> 
-  mutate(longSepal = Sepal.Length >= 2  |>  Sepal.Width)
+  mutate(longSepal = Sepal.Length >= 2 * Sepal.Width)
 
 # Summarise the dataset by species and calculate the following:
 # - Count in how many cases longSepal is TRUE (R treats TRUE as 1 and FALSE as 0)
@@ -53,11 +57,16 @@ part2 |>  group_by(Species) |>
 
 #....... HUMANOIDs DATSET ....... 
 #................................
+# This dataset is adapted from
 # https://www.kaggle.com/datasets/santiago123678/evolution-of-humans-datasets-for-clasification
 
-# Load the large humanoids.xlsx dataset and try to find out the following
-# about homo sapiens (our species of human) based on the data
-# - what are the major differences / similarities between the oldest and most recent species
-# - which other humanoid species are the most closely related to us and is it just time?
-# - are species from the same region or habitat more likely to share similar characteristics?
+# Download the adapted dataset from https://go.pjvan.camp/s2rdata1
+#  and import the csv file as a data frame.
 
+# Try to find out the following about homo sapiens (our species of human) based on the data
+# - what are the major differences / similarities between the oldest and most recent species?
+#    Note that the time is denoting how many millions of years ago the specimen lived
+# - which other humanoid species are the most closely related to us?
+#    Which features are most indicative of being more recent 
+# - are species from the same region or habitat more likely to share similar characteristics?
+# - Explore differences in diversity between, and age within species in the same location
